1566f415944e4ef05c24b18ec44ad029347615fb
Alex Rodríguez <rodriguezsm2013@licifug.ugto.mx>
2021-11-11 16:19:02 -0600

    reploy working

--- server.py
+++ server.py
@@ -2,25 +2,24 @@ import joblib
 import numpy as np
 
 from flask import Flask
-from flask import jsonify, request, render_template
+from flask import jsonify, request, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 #POSTMAN para pruebas
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-	if request.method == 'POST':
-		user_input = request.form.get(['high', 'low', 'gdp', 'family', 'lifexp', 'freedom', 'generosity', 'corruption', 'dystopia'])
-		prediction = model.predict([user_input])
-		for i in user_input:
-			print(i)
-		return "Hola"	
-	else:
+# Datos de prueba
+# [7.594444821,7.479555538,1.616463184,1.53352356,0.796666503,0.635422587,0.362012237,0.315963835,2.277026653]
+	int_features = [float(x) for x in request.form.values()]
+	features = np.array(int_features)
+	print(int_features)
+	print(features)
+	prediction = model.predict(features.reshape(1, -1))
+	
+	return render_template('index.html', pred=prediction)    
 
-	    X_test = np.array([7.594444821,7.479555538,1.616463184,1.53352356,0.796666503,0.635422587,0.362012237,0.315963835,2.277026653])
-	    prediction = model.predict(X_test.reshape(1, -1))
-	    #return jsonify({'Prediction' : list(prediction)})
-	    return render_template('index.html', prediction=list(prediction))
+
 if __name__ == "__main__":
     model = joblib.load("./models/best_model.pkl")
-    app.run(port=8080)
+    app.run(debug=True, port=8080)
