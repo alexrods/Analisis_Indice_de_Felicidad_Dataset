bd40340b6519179a33f274e56d54f6fbf16fd19f
Alex Rodríguez <rodriguezsm2013@licifug.ugto.mx>
2022-01-11 23:38:50 -0600

    improve frontend desing

--- server.py
+++ server.py
@@ -10,15 +10,17 @@ app = Flask(__name__)
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
 # Datos de prueba
-# [7.594444821,7.479555538,1.616463184,1.53352356,0.796666503,0.635422587,0.362012237,0.315963835,2.277026653]
-	int_features = [float(x) for x in request.form.values()]
-	features = np.array(int_features)
-	print(int_features)
-	print(features)
-	prediction = model.predict(features.reshape(1, -1))
+	# features = [7.594444821,7.479555538,1.616463184,1.53352356,0.796666503,0.635422587,0.362012237,0.315963835,2.277026653]
+	if request.method == "POST":
 	
-	return render_template('index.html', pred=prediction)    
+		int_features = [float(x) for x in request.form.values()]
+		features = np.array(int_features).reshape(1,-1)
+		prediction = model.predict(features)
+	
+		return render_template('index.html', pred=f"El Score de felicidad es: {round(prediction[0], 4)}")
+	
 
+	return render_template('index.html')
 
 if __name__ == "__main__":
     model = joblib.load("./models/best_model.pkl")
